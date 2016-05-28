58a5379160175b7f71bb47e0a7893ef96ea13762
Witk32 <witk32@qq.com>
2016-05-28 17:18:39 +0800

    1.0

---
+++ src/git_MHL/index.java
@@ -0,0 +1,39 @@
+package git_MHL;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class index {
+	static FileReader fr=null;
+	static BufferedReader bf=null;
+
+	public static void main(String[] args) {
+		try {
+			fr=new FileReader("src/db/db_user");
+			bf=new BufferedReader(fr);
+			String s="";
+			String id="";
+			String co="";
+			id=bf.readLine();
+			co=bf.readLine();
+			System.out.println("id:"+id+",code:"+co);
+			
+		} catch (Exception e1) {
+			e1.printStackTrace();
+	}
+			finally{
+			try {
+				fr.close();
+				bf.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
+
+}
